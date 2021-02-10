bd0d615d3b92c8bfa5c1f7ddf619cf8ea3412016
Alberto <alberto.delafuente1999@gmail.com>
2021-02-10 01:45:52 +0100

    Implementación Geolocalización Implementacion BASICA de la base de datos Inserción BASICA en la base de datos
    (RECARGAR EL build.gradle)

--- app/src/main/java/com/example/hideout/CrearReto.java
+++ app/src/main/java/com/example/hideout/CrearReto.java
@@ -1,28 +1,34 @@
 package com.example.hideout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
-import android.content.Context;
 import android.content.Intent;
+
 import android.graphics.Bitmap;
-import android.location.LocationManager;
+
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
-import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
-public class CrearReto extends AppCompatActivity  implements View.OnClickListener {
-
+public class CrearReto extends AppCompatActivity implements View.OnClickListener {
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    EditText pista = findViewById(R.id.eTPista);
+    double latitud;
+    double longitud;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_reto);
-
-
+        ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION}, 1);
         findViewById(R.id.bSubirFotoReto).setOnClickListener(this);
         findViewById(R.id.imageBack).setOnClickListener(this);
     }
@@ -42,6 +48,12 @@ public class CrearReto extends AppCompatActivity  implements View.OnClickListene
                 finish();
 
                 break;
+
+            case R.id.bCrearReto:
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("message");
+                myRef.setValue("Hello, World!");
+                break;
         }
     }
 
@@ -55,8 +67,9 @@ public class CrearReto extends AppCompatActivity  implements View.OnClickListene
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            LocationTrack coordenadas = new LocationTrack(this);
-            System.out.println("Las coordenadas son: "+coordenadas.getLatitude());
+            LocationTrack locationTrack = new LocationTrack(this);
+            latitud=locationTrack.getLatitude();
+            longitud=locationTrack.getLongitude();
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             ImageView previewImagen = findViewById(R.id.imgReto);
