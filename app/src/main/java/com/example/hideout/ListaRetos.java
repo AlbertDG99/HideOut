0a94702cc4922be51a9ef891777d5b175db3f830
Alberto <alberto.delafuente1999@gmail.com>
2021-02-23 16:55:47 +0100

    SOLO MUESTRA LOS DE <400

--- app/src/main/java/com/example/hideout/ListaRetos.java
+++ app/src/main/java/com/example/hideout/ListaRetos.java
@@ -38,7 +38,7 @@ public class ListaRetos extends AppCompatActivity implements View.OnClickListene
 
     private double latitudUsu;
     private double longitudUsu;
-    private Location locUsu= new Location("LocUsu");
+    private Location locUsu = new Location("LocUsu");
     RetosAdapter adapter;
 
     @Override
@@ -64,7 +64,7 @@ public class ListaRetos extends AppCompatActivity implements View.OnClickListene
 
         //final ArrayAdapter<Reto> adapter = new ArrayAdapter<Reto>(this, android.R.layout.simple_dropdown_item_1line, retosArrayList);
 
-         adapter = new RetosAdapter(this, retosArrayList);
+        adapter = new RetosAdapter(this, retosArrayList);
 
         myRef.addListenerForSingleValueEvent(new ValueEventListener() {
 
@@ -81,7 +81,7 @@ public class ListaRetos extends AppCompatActivity implements View.OnClickListene
 
                     }
                 }
-               retosArrayList= organizarLista(retosArrayList);
+                retosArrayList = organizarLista(retosArrayList);
                 adapter.notifyDataSetChanged();
             }
 
@@ -113,7 +113,7 @@ public class ListaRetos extends AppCompatActivity implements View.OnClickListene
     private ArrayList<Reto> organizarLista(ArrayList<Reto> retosArrayList) {
         Boolean error = true;
         while (error) {
-            error=false;
+            error = false;
             float dAnterior = 0;
             for (int i = 0; i < retosArrayList.size(); i++) {
                 Reto r = retosArrayList.get(i);
@@ -123,17 +123,21 @@ public class ListaRetos extends AppCompatActivity implements View.OnClickListene
                 actual.setLongitude(r.getLongitud());
 
                 float dActual = actual.distanceTo(locUsu);
-                if (dActual < dAnterior && i != 0) {
+                if (dActual > 400) {
+                    retosArrayList.remove(i);
+                    i--;
+                } else if (dActual < dAnterior && i != 0) {
                     Reto aux = retosArrayList.get(i - 1);
                     retosArrayList.set(i - 1, r);
                     retosArrayList.set(i, aux);
                     error = true;
+                    dAnterior=dActual;
                 }
-                dAnterior = dActual;
+
 
             }
         }
-        return  retosArrayList;
+        return retosArrayList;
     }
 
     @Override
@@ -148,65 +152,6 @@ public class ListaRetos extends AppCompatActivity implements View.OnClickListene
 
                 break;
 
-        }
-    }
-    /**
-     * Release memory when the UI becomes hidden or when system resources become low.
-     * @param level the memory-related event that was raised.
-     */
-    public void onTrimMemory(int level) {
-
-        // Determine which lifecycle or system event was raised.
-        switch (level) {
-
-            case ComponentCallbacks2.TRIM_MEMORY_UI_HIDDEN:
-
-                    /*
-                       Release any UI objects that currently hold memory.
-
-                       The user interface has moved to the background.
-                    */
-
-                break;
-
-            case ComponentCallbacks2.TRIM_MEMORY_RUNNING_MODERATE:
-            case ComponentCallbacks2.TRIM_MEMORY_RUNNING_LOW:
-            case ComponentCallbacks2.TRIM_MEMORY_RUNNING_CRITICAL:
-
-                    /*
-                       Release any memory that your app doesn't need to run.
-
-                       The device is running low on memory while the app is running.
-                       The event raised indicates the severity of the memory-related event.
-                       If the event is TRIM_MEMORY_RUNNING_CRITICAL, then the system will
-                       begin killing background processes.
-                    */
-
-                break;
-
-            case ComponentCallbacks2.TRIM_MEMORY_BACKGROUND:
-            case ComponentCallbacks2.TRIM_MEMORY_MODERATE:
-            case ComponentCallbacks2.TRIM_MEMORY_COMPLETE:
-
-                    /*
-                       Release as much memory as the process can.
-
-                       The app is on the LRU list and the system is running low on memory.
-                       The event raised indicates where the app sits within the LRU list.
-                       If the event is TRIM_MEMORY_COMPLETE, the process will be one of
-                       the first to be terminated.
-                    */
-
-                break;
-
-            default:
-                    /*
-                      Release any non-critical data structures.
-
-                      The app received an unrecognized memory level value
-                      from the system. Treat this as a generic low-memory message.
-                    */
-                break;
         }
     }
 }
