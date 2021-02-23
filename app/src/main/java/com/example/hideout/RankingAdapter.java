fe7595413513e17f03319fe632a14d31b953998d
Solrac <elbuzondecarloscr@gmail.com>
2021-02-23 18:01:04 +0100

    adapter completo

--- app/src/main/java/com/example/hideout/RankingAdapter.java
+++ app/src/main/java/com/example/hideout/RankingAdapter.java
@@ -1,8 +1,6 @@
 package com.example.hideout;
 
 import android.content.Context;
-import android.graphics.BitmapFactory;
-import android.location.Location;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,15 +10,15 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 
 public class RankingAdapter  extends ArrayAdapter<Usuario> {
 
+    ImageView imagen;
+
+    private static int cont = 0;
 
     public RankingAdapter(@NonNull Context context,  @NonNull ArrayList<Usuario> usuarios) {
         super(context, 0, usuarios);
@@ -37,7 +35,7 @@ public class RankingAdapter  extends ArrayAdapter<Usuario> {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.layout_ranking, parent, false);
         }
         // Lookup view for data population
-        ImageView imagen = (ImageView) convertView.findViewById(R.id.imagenMed);
+        imagen = (ImageView) convertView.findViewById(R.id.imagenMed);
         TextView textSuperiorRank = (TextView) convertView.findViewById(R.id.textSuperiorRank);
         TextView textInferiorRank = (TextView) convertView.findViewById(R.id.textInferiorRank);
 
@@ -46,9 +44,29 @@ public class RankingAdapter  extends ArrayAdapter<Usuario> {
         assert usuario != null;
         textSuperiorRank.setText(usuario.getIdUsu());
         textInferiorRank.setText(usuario.getMonedas());
+        imagenMedallas();
+
+        cont++;
 
         // Return the completed view to render on screen
         return convertView;
     }
 
+    private void imagenMedallas(){
+
+        switch(cont){
+            case 0:
+                imagen.setImageResource(R.drawable.goldicon);
+                break;
+            case 1:
+                imagen.setImageResource(R.drawable.silvericon);
+                break;
+            case 2:
+                imagen.setImageResource(R.drawable.bronceicon);
+                break;
+            default:
+                imagen.setImageResource(R.drawable.medallaicon);
+                break;
+        }
+    }
 }
